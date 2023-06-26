ef8dbfb158ce25f181a95c065727eda4654e499d
Dae-woon Park <5886230+devvace@users.noreply.github.com>
2023-06-26 14:42:38 +0900

    영상 통화 작업하기

--- lib/screen/cam_screen.dart
+++ lib/screen/cam_screen.dart
@@ -1,39 +1,117 @@
+import 'package:agora_rtc_engine/agora_rtc_engine.dart';
 import 'package:flutter/material.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:video_call_v6/const/agora.dart';
 
-class CamScreen extends StatelessWidget {
+class CamScreen extends StatefulWidget {
   const CamScreen({Key? key}) : super(key: key);
+
+  @override
+  State<CamScreen> createState() => _CamScreenState();
+}
+
+class _CamScreenState extends State<CamScreen> {
+  RtcEngine? engine;
+
+  // 내 ID
+  int? uid = 0;
+
+  // 상대 ID
+  int? otherUid;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('LIVE'),
+        title: const Text('LIVE'),
       ),
       body: FutureBuilder<bool>(
-        future: init(),
-        builder: (context, snapshot) {
-          if(snapshot.hasError) {
-            return Center(
-              child: Text(
-                snapshot.error.toString(),
-              ),
-            );
-          }
+          future: init(),
+          builder: (context, snapshot) {
+            if (snapshot.hasError) {
+              return Center(
+                child: Text(
+                  snapshot.error.toString(),
+                ),
+              );
+            }
 
-          if(!snapshot.hasData) {
-            return Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-          return Column(
-            children: [
+            if (!snapshot.hasData) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Expanded(
+                  child: Stack(
+                    children: [
+                      renderMainView(),
+                      Align(
+                        alignment: Alignment.topLeft,
+                        child: Container(
+                          color: Colors.grey,
+                          height: 160,
+                          width: 120,
+                          child: renderSubView(),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                  child: ElevatedButton(
+                    onPressed: () async {
+                      if (engine != null) {
+                        await engine!.leaveChannel();
+                        engine = null;
+                      }
 
-            ],
-          );
-        }
-      ),
+                      Navigator.of(context).pop();
+                    },
+                    child: Text('채널 나가기'),
+                  ),
+                ),
+              ],
+            );
+          }),
     );
+  }
+
+  renderMainView() {
+    if (uid == null) {
+      return const Center(
+        child: Text('채널에 참여해주세요.'),
+      );
+    } else {
+      // 채널에 참여하고 있을 때
+      return AgoraVideoView(
+        controller: VideoViewController(
+          rtcEngine: engine!,
+          canvas: const VideoCanvas(
+            uid: 0,
+          ),
+        ),
+      );
+    }
+  }
+
+  renderSubView() {
+    if (otherUid == null) {
+      return const Center(
+        child: Text('채널에 유저가 없습니다.'),
+      );
+    } else {
+      return AgoraVideoView(
+        controller: VideoViewController.remote(
+          rtcEngine: engine!,
+          canvas: VideoCanvas(uid: otherUid),
+          connection: const RtcConnection(channelId: CHANNEL_NAME),
+        ),
+      );
+    }
   }
 
   Future<bool> init() async {
@@ -42,11 +120,81 @@ class CamScreen extends StatelessWidget {
     final cameraPermission = resp[Permission.camera];
     final microphonePermission = resp[Permission.microphone];
 
-    if(cameraPermission != PermissionStatus.granted ||
-    microphonePermission != PermissionStatus.granted) {
+    if (cameraPermission != PermissionStatus.granted ||
+        microphonePermission != PermissionStatus.granted) {
       throw '카메라 또는 마이크 권한이 없습니다.';
     }
 
+    if (engine == null) {
+      engine = createAgoraRtcEngine();
+
+      await engine!.initialize(
+        const RtcEngineContext(
+          appId: APP_ID,
+        ),
+      );
+
+      engine!.registerEventHandler(
+        RtcEngineEventHandler(
+            // 내가 채널에 입장했을 때
+            // connection -> 연결 정보
+            // elapsed -> 연결된 시간(연결된지 얼마나 됐는지)
+            onJoinChannelSuccess: (RtcConnection connection, int elapsed) {
+          debugPrint('채널에 입장했습니다. uid: ${connection.localUid}');
+          setState(() {
+            uid = connection.localUid;
+          });
+        },
+            // 내가 채널에서 나갔을 때
+            onLeaveChannel: (RtcConnection connection, RtcStats stats) {
+          debugPrint('채널 퇴장');
+          setState(() {
+            uid == null;
+          });
+        },
+            // 상대가 들어왔을 때
+            onUserJoined:
+                (RtcConnection connection, int remoteUid, int elapsed) {
+          debugPrint('상대가 채널에 입장했습니다. otherUid: $remoteUid');
+          setState(() {
+            otherUid = remoteUid;
+          });
+        },
+            // 상대가 나갔을 때
+            onUserOffline: (RtcConnection connection, int remoteUid,
+                UserOfflineReasonType reason) {
+          debugPrint('상대가 채널에서 나갔습니다. otherUid: $remoteUid');
+          setState(() {
+            otherUid = null;
+          });
+        }),
+      );
+
+      await engine!.enableVideo();
+
+      await engine!.startPreview();
+
+      ChannelMediaOptions options = const ChannelMediaOptions();
+
+      await engine!.joinChannel(
+        token: TEMP_TOKEN,
+        channelId: CHANNEL_NAME,
+        uid: 0,
+        options: options,
+      );
+    }
+
     return true;
+  }
+
+  @override
+  void dispose() async {
+    if(engine != null) {
+      await engine!.leaveChannel(
+        options: const LeaveChannelOptions(),
+      );
+      engine!.release();
+    }
+    super.dispose();
   }
 }
