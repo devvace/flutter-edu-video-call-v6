31b31f351e09c8ddd071e8a387917f3e30aac148
Dae-woon Park <5886230+devvace@users.noreply.github.com>
2023-06-26 11:06:53 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# video_call_v6
+Agora API를 이용한 영상 통화 프로젝트(Sample)
+
+## Getting Started
+- 사용 패키지
+  - agora_rtc_engine
+  - permission_handler
---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:video_call_v6/screen/home_screen.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: HomeScreen(),
+    ),
+  );
+}
