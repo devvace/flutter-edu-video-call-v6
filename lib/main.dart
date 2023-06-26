15aa75172441f61147a91c1f7d842857258f3bb7
Dae-woon Park <5886230+devvace@users.noreply.github.com>
2023-06-26 12:16:27 +0900

    - Agora, Permission handler 버전 최근 버전으로 변경 - main.dart에서 폰트 적용

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,10 @@ import 'package:video_call_v6/screen/home_screen.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
+    MaterialApp(
+      theme: ThemeData(
+        fontFamily: 'NotoSans',
+      ),
       home: HomeScreen(),
     ),
   );
